f1eb5b0fdf88c5a304d1c0cf21f98a6ca4f19ddc
luisefigenio-sketch <luis.efigenio@escola.pr.gov.br>
2026-07-07 10:35:46 -0300

    .

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ botoes.forEach(function (botao){
 
     function botaoClicado(){
         console.log("botao clicado")
+        ler texto = botao.querySelector("span")
+        texto.textContent++
     }
     
 })
